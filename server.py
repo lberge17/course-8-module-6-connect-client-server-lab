39cb7a75d595acfca1f3e0b0e4eee8d3eaf4e8af
Laura Berge <lauraeberge@gmail.com>
2025-08-12 13:48:03 -0500

    solution

--- server.py
+++ server.py
@@ -6,12 +6,22 @@ CORS(app)
 
 # Create a list called 'events' with a couple of sample event dictionaries
 # Each dictionary should have an 'id' and a 'title'
+events = [
+    {"id": 1, "title": "Homecoming"},
+    {"id": 2, "title": "Concert"}
+]
 
 # TASK: Create a route for "/"
 # This route should return a JSON welcome message
+@app.route("/", methods=["GET"])
+def welcome():
+    return jsonify({"message": "Welcome"}), 200
 
 # TASK: Create a GET route for "/events"
 # This route should return the full list of events as JSON
+@app.route("/events", methods=["GET"])
+def get_events():
+    return jsonify(events), 200
 
 # TASK: Create a POST route for "/events"
 # This route should:
@@ -20,6 +30,16 @@ CORS(app)
 # 3. Create a new event with a unique ID and the provided title
 # 4. Add the new event to the events list
 # 5. Return the new event with status code 201
+@app.route("/events", methods=["POST"])
+def add_event():
+    data = request.get_json()
+    title = data.get("title")
+    if  not title:
+        return jsonify({"message": "Missing event title in request"}), 422
+    new_id = max([e["id"] for e in events], default=0) + 1
+    new_event = {"id": new_id, "title": title}
+    events.append(new_event)
+    return jsonify(new_event), 201
 
 if __name__ == "__main__":
     app.run(debug=True)
